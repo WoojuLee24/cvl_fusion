43434f382bc69c379f5add894b168f137cc6416f
woojulee24 <bigtree123433@gmail.com>
2024-09-01 15:17:13 +0000

    debug metric

--- pixloc/pixlib/models/two_view_refiner2dv2.py
+++ pixloc/pixlib/models/two_view_refiner2dv2.py
@@ -788,8 +788,10 @@ class TwoViewRefiner2D(BaseModel):
 
         @torch.no_grad()
         def scaled_pose_error(T_q2r):
-            err_R, err_t = (T_r2q_gt@T_q2r).magnitude()
-            err_lat, err_long = (T_r2q_gt@T_q2r).magnitude_latlong()
+            # err_R, err_t = (T_r2q_gt@T_q2r).magnitude()
+            # err_lat, err_long = (T_r2q_gt@T_q2r).magnitude_latlong()
+            err_R, err_t = (T_q2r @ T_r2q_gt).magnitude()
+            err_lat, err_long = (T_q2r @ T_r2q_gt).magnitude_latlong()
             return err_R, err_t, err_lat, err_long
 
         metrics = {}
